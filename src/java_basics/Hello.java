49eebb595ac32c6d03adc9e193fc3c1f07649cd4
Angelo <angelomoura@gmail.com>
2024-08-06 09:51:01 -0300

    'init'

---
+++ src/java_basics/Hello.java
@@ -0,0 +1,11 @@
+package java_basics;
+
+public class Hello {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated constructor stub
+		System.out.println("Hello World!");
+		System.out.print("Teste");
+	}
+
+}
