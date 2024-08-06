3675c4a9ce8ad0a9682fffb7d1d575ae83fd3915
Angelo <angelomoura@gmail.com>
2024-08-06 16:48:02 -0300

    'variable'

--- src/java_basics/VariablesContinued.java
+++ src/java_basics/VariablesContinued.java
@@ -4,7 +4,15 @@ public class VariablesContinued {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated constructor stub
+		System.out.println("Still Learning about Java Variables!");
 		
+		/*
+		 * About variables resolution.
+		 */
+		
+		/*
+		 * An INT variable has 32 bits resolution. It means that a INT variable can be 2**31
+		 */
 	}
 
 }
