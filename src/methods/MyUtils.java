375d6c115f10737cefe6431406e0a50a7ce6f186
Angelo Moura <angelomoura@gmail.com>
2026-04-14 13:34:56 -0300

    Including document comments

--- src/methods/MyUtils.java
+++ src/methods/MyUtils.java
@@ -1,34 +1,68 @@
 package methods;
 
 class MyUtils {
+	/**
+	 * 
+	 * @param argument
+	 */
 	public static void printAnotherJunk(int argument) {
 		System.out.println("From MyUtils: Integer ==> " + argument);
 	}
 
+	/**
+	 * 
+	 * @param argument
+	 */
 	public static void printAnotherJunk(String argument) {
 		System.out.println("From MyUtils: String ==> " + argument);
 	}
 
+	/**
+	 * 
+	 * @param argument
+	 * @return
+	 */
 	public static String returnJunk(String argument) {
 		return ("From MyUtils: return String ==> " + argument);
 	}
 
+	/**
+	 * 
+	 * @param argument
+	 * @return
+	 */
 	public static int returnJunk(int argument) {
 		return (argument * 100);
 	}
 
+	/**
+	 * @param firstArg
+	 * @param secondArg
+	 * @return
+	 */
 	public static int sumTwoNumbers(int firstArg, int secondArg) {
 		return firstArg + secondArg;
 	}
 
+	/**
+	 * @param argument
+	 * @return
+	 */
 	private static int privateReturnInteger(int argument) {
 		return argument;
 	}
 
+	/**
+	 * 
+	 */
 	public static void printPrivateReturnInteger() {
 		System.out.println("Value returned by a private method I called: " + privateReturnInteger(1456));
 	}
 
+	/**
+	 * @param argumentA
+	 * @return
+	 */
 	public int add10(int argumentA) {
 		return argumentA + 10;
 	}
