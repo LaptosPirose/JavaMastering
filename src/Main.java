905d20c34bbf6d50ced1ebed0f8342ad6ca696fc
Angelo Moura <angelomoura@gmail.com>
2026-04-15 08:11:33 -0300

    Deleting Java Resources

--- src/Main.java
+++
@@ -1,6 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Este é o acento correto! Olá__! Impressão");
-
-    }
-}
---
+++ src/padrao/Main.java
@@ -0,0 +1,7 @@
+package padrao;
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Este é o acento correto! Olá__! Impressão");
+
+    }
+}
